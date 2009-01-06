45c266b9788658ae523647ebbefd18d5c1f045be
vti <viacheslav.t@gmail.com>
2009-01-06 02:19:20 +0100

    validation is back

--- lib/ObjectDB/Validation.pm
+++ lib/ObjectDB/Validation.pm
@@ -36,7 +36,14 @@ sub is_valid {
 
     my $errors = 0;
     foreach my $col (@_ ? @_ : $self->meta->columns) {
+        my $options = $self->meta->_columns->{$col};
+
         $errors++ unless _is_valid_null($self, $col);
+
+        if (%$options) {
+            $errors++ unless _is_valid_length($self, $col);
+            $errors++ unless _is_valid_regex($self, $col);
+        }
 
         if ($caller eq 'create' || $caller eq 'update') {
             $errors++ if !$errors && !_is_valid_unique($self, $col);
@@ -84,6 +91,54 @@ sub _is_valid_null {
         $self->errors($col => 'null');
         warn "$col is required" if DEBUG;
         return 0;
+    }
+
+    return 1;
+}
+
+sub _is_valid_regex {
+    my $self = shift;
+    my $col  = shift;
+
+    if (my $regex = $self->meta->_columns->{$col}->{regex}) {
+        unless ($self->column($col) =~ qr/^$regex$/) {
+            $self->errors($col => 'regex');
+            warn "$col regex failed" if DEBUG;
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+sub _is_valid_length {
+    my $self = shift;
+    my $col = shift;
+
+    if (my $length = $self->meta->_columns->{$col}->{length}) {
+        my $max_length;
+        my $min_length;
+        if (ref $length eq 'ARRAY') {
+            $min_length = $length->[0];
+            $max_length = $length->[1];
+        } else {
+            $min_length = 0;
+            $max_length = $length;
+        }
+
+        if (not defined $self->column($col)) {
+            return 1 if $min_length == 0;
+
+            $self->errors($col => 'length');
+            return 0;
+        }
+
+        if (   length $self->column($col) < $min_length
+            || length $self->column($col) > $max_length)
+        {
+            $self->errors($col => 'length');
+            return 0;
+        }
     }
 
     return 1;
